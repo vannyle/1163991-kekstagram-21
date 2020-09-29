b35407f935acb0def9c5b7ae957f9ac0e3aaefb1
vannnyle@gmail.com <vannnyle@gmail.com>
2020-09-29 16:40:28 +0300

    Refactoring coding structure

--- js/main.js
+++ js/main.js
@@ -2,10 +2,8 @@
 //
 // Variables
 //
-const arrNumberUrl = getRandom(1, 25);
-const arrNumberLikes = getRandom(15, 200);
-const arrNumberUsers = getRandom(1, 6);
-const pictureDescription = [
+
+const PHOTO_DESCRIPTIONS = [
   `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsam?`,
   `Lorem ipsum dolor sit amet.`,
   `Lorem ipsum.`,
@@ -15,68 +13,87 @@ const pictureDescription = [
   `Lorem ipsum dolor sit amet, consectetur adipisicing`,
 ];
 
-const users = {
-  avatars: arrNumberUsers,
-  messages: [
-    `Всё отлично!`,
-    `В целом всё неплохо. Но не всё.`,
-    `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
-    `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
-    `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
-    `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`,
-  ],
-  names: [`Артем`, `Иван`, `Петров`, `Наталия`, `София`, `Екатерина`, `Николай`],
-};
-
+const MESSAGES = [
+  `Всё отлично!`,
+  `В целом всё неплохо. Но не всё.`,
+  `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
+  `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
+  `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
+  `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`,
+];
+const USER_NAMES = [`Артем`, `Иван`, `Петров`, `Наталия`, `София`, `Екатерина`, `Николай`];
+const IMAGES_COUNT = 25;
 const pictures = document.querySelector(`.pictures`);
 const pictureTemplate = document.querySelector(`#picture`).content.querySelector(`.picture`);
 
 //
 // Functions
 //
-function getRandomFrom(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
 
+/**
+ * Returns random integer in a range
+ * @param {number} min
+ * @param {number} max
+ * @return {number}
+ */
 function getRandom(min, max) {
-  const randomNumbers = [];
-  for (let i = min; i <= max; i++) {
-    const temp = Math.floor(Math.random() * (1 + max - min)) + min;
-    if (randomNumbers.indexOf(temp) === -1) {
-      randomNumbers.push(temp);
-    } else {
-      i--;
-    }
-  }
-  return randomNumbers;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function generateComments() {
-  const userComments = [];
-  for (let i = 0; i < getRandomFrom(arrNumberUsers); i++) {
-    const userComment = {
-      avatar: `img/avatar-${getRandomFrom(users.avatars)}.svg`,
-      message: getRandomFrom(users.messages),
-      name: getRandomFrom(users.names),
-    };
-    userComments.push(userComment);
-  }
-  return userComments;
+/**
+ * Returns random element from given array
+ * @param {Array} arr
+ * @return {*}
+ */
+function getRandomFromArray(arr) {
+  return arr[getRandom(0, arr.length)];
 }
+
+/**
+ * Returns shuffled array with unique integers from `min` to `max`
+ * @param {number} min
+ * @param {number} max
+ * @return {[number]}
+ */
+function getRandomizedArray(min, max) {
+  const arr = [];
+  for (let i = min; i <= max; i++) {
+    arr.push(i);
+  }
+  // Shuffle array
+  return arr.sort(() => Math.random() - 0.5);
+}
+
 
 function generatePicture() {
   const photos = [];
-  for (let i = 0; i < arrNumberUrl.length; i++) {
+  const shuffledImagesIdx = getRandomizedArray(1, IMAGES_COUNT);
+
+  for (let i = 0; i < IMAGES_COUNT; i++) {
     const comments = generateComments();
     const photo = {
-      url: `photos/${arrNumberUrl[i]}.jpg`,
-      description: getRandomFrom(pictureDescription),
-      likes: arrNumberLikes[i],
+      url: `photos/${shuffledImagesIdx[i]}.jpg`,
+      description: getRandomFromArray(PHOTO_DESCRIPTIONS),
+      likes: getRandom(15, 200),
       comment: comments.length,
     };
     photos.push(photo);
   }
   return photos;
+}
+
+function generateComments() {
+  const userComments = [];
+  const commentsCount = getRandom(1, 50);
+  for (let i = 0; i < commentsCount; i++) {
+    const userComment = {
+      avatar: `img/avatar-${getRandom(1, 6)}.svg`,
+      message: getRandomFromArray(MESSAGES),
+      name: getRandomFromArray(USER_NAMES),
+    };
+    userComments.push(userComment);
+  }
+  return userComments;
 }
 
 function renderPicture(picture) {
@@ -87,7 +104,7 @@ function renderPicture(picture) {
   return pictureElement;
 }
 
-function createFragment() {
+function createFragment(photos) {
   const fragment = document.createDocumentFragment();
   for (let i = 0; i < photos.length; i++) {
     fragment.appendChild(renderPicture(photos[i]));
@@ -103,5 +120,5 @@ const photos = generatePicture();
 //
 // Presentation
 //
-pictures.appendChild(createFragment());
+pictures.appendChild(createFragment(photos));
 
