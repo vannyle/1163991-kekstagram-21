17024d6388157914f0d6e6be27dea1b3de5ae618
vannnyle@gmail.com <vannnyle@gmail.com>
2020-10-10 12:40:29 +0300

    Set the hashtags validation

--- js/main.js
+++ js/main.js
@@ -66,6 +66,7 @@ const effectImages = imageUploadOverlay.querySelector(`.effects`);
 const sliderEffectPin = imageUploadOverlay.querySelector(`.effect-level__pin`);
 const sliderEffectLine = imageUploadOverlay.querySelector(`.effect-level__line`);
 const sliderEffectDepth = imageUploadOverlay.querySelector(`.effect-level__depth`);
+const hashtagsInput = imageUploadOverlay.querySelector(`.text__hashtags`);
 
 // Functions
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
@@ -156,7 +157,6 @@ effectImages.addEventListener(`change`, (e) => {
   effect = EFFECTS[target.value];
   if (effect) {
     setEffectValue(1);
-
     if (effect.prop === `none`) {
       document.querySelector(`.effect-level`).style.visibility = `hidden`;
     } else {
@@ -164,7 +164,6 @@ effectImages.addEventListener(`change`, (e) => {
     }
   }
 });
-
 
 const setEffectValue = (value) => {
   sliderEffectPin.style.left = `${value * 100}%`;
@@ -192,7 +191,7 @@ sliderEffectPin.addEventListener(`mousedown`, function (evt) {
       x: moveEvt.clientX,
     };
     const newCoordsPercent = (sliderEffectPin.offsetLeft - shift.x) / SLIDER_WIDTH;
-    if (newCoordsPercent > 0 && newCoordsPercent < 1) {
+    if (newCoordsPercent >= 0 && newCoordsPercent <= 1) {
       setEffectValue(newCoordsPercent);
     }
   };
@@ -212,5 +211,33 @@ sliderEffectPin.addEventListener(`mousedown`, function (evt) {
 
   document.addEventListener(`mousemove`, onMouseMove);
   document.addEventListener(`mouseup`, onMouseUp);
+});
+
+// Hashtags validation
+
+const MIN_NAME_LENGTH = 2;
+const MAX_NAME_LENGTH = 20;
+const HASHTAGS_COUNT = 5;
+
+hashtagsInput.addEventListener(`input`, (e) => {
+  const arrayOfHashtags = hashtagsInput.value.split(` `);
+
+  const pattern = /(?:\s|^)#[A-Za-z0-9\-\.\_]+(?:\s|$)/;
+  arrayOfHashtags.forEach((hashtag) => {
+    if (hashtag.length < MIN_NAME_LENGTH) {
+      hashtagsInput.setCustomValidity(`Ещё ${(MIN_NAME_LENGTH - hashtag.length)} симв.`);
+    } else if (hashtag.length > MAX_NAME_LENGTH) {
+      hashtagsInput.setCustomValidity(`Удалите лишние ${(hashtag.length - MAX_NAME_LENGTH)} симв.`);
+    } else if (!pattern.test(hashtagsInput.value)) {
+      hashtagsInput.setCustomValidity(`Хэштеги начинаются с # не должны содержать символы и пробелы`);
+    } else {
+      hashtagsInput.setCustomValidity(``);
+    }
+    if (arrayOfHashtags.length > HASHTAGS_COUNT) {
+      hashtagsInput.setCustomValidity(`Не больше пяти хэштегов`);
+    }
+  });
+  hashtagsInput.reportValidity();
+  return true;
 });
 
