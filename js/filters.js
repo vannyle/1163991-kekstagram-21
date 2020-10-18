a22f904c67f560608fe7e98ebc16159b0e63bf31
vannnyle@gmail.com <vannnyle@gmail.com>
2020-10-18 15:01:43 +0300

    Move filters listener to separate module

--- js/filters.js
+++ js/filters.js
@@ -1,8 +1,38 @@
 'use strict';
 
 (() => {
-  window.filters = () => {
-    const imgFilters = document.querySelector(`.img-filters`);
-    imgFilters.classList.remove(`img-filters--inactive`);
+  const imgFilters = document.querySelector(`.img-filters`);
+  const filterDefault = document.getElementById(`filter-default`);
+  const filterRandom = document.getElementById(`filter-random`);
+  const filterDiscussed = document.getElementById(`filter-discussed`);
+  const RANDOM_PICTURES_COUNT = 10;
+  const ACTIVE_FILTER_CLASS = `img-filters__button--active`;
+
+  imgFilters.classList.remove(`img-filters--inactive`);
+
+  const setActiveFilter = (btn) => {
+    document.querySelector(`.${ACTIVE_FILTER_CLASS}`).classList.remove(ACTIVE_FILTER_CLASS);
+    btn.classList.add(ACTIVE_FILTER_CLASS);
+  };
+  const setFiltersHandler = (initialData) => {
+    filterDefault.addEventListener(`click`, () => {
+      window.gallery.renderGallery(initialData);
+      setActiveFilter(filterDefault);
+    });
+
+    filterRandom.addEventListener(`click`, () => {
+      const randomPhotoJson = window.utils.getRandomizedArray(initialData).slice(0, RANDOM_PICTURES_COUNT);
+      window.gallery.renderGallery(randomPhotoJson);
+      setActiveFilter(filterRandom);
+    });
+
+    filterDiscussed.addEventListener(`click`, () => {
+      const sortedData = window.utils.getSortedCommentArr(initialData);
+      window.gallery.renderGallery(sortedData);
+      setActiveFilter(filterDiscussed);
+    });
+  };
+  window.filters = {
+    setFiltersHandler,
   };
 })();
--- js/gallery.js
+++ js/gallery.js
@@ -2,45 +2,21 @@
 
 (() => {
   let initialData = [];
-  const filterDefault = document.getElementById(`filter-default`);
-  const filterRandom = document.getElementById(`filter-random`);
-  const filterDiscussed = document.getElementById(`filter-discussed`);
-  const RANDOM_PICTURES_COUNT = 10;
-  const ACTIVE_FILTER_CLASS = `img-filters__button--active`;
 
-  const setActiveFilter = (btn) => {
-    document.querySelector(`.${ACTIVE_FILTER_CLASS}`).classList.remove(ACTIVE_FILTER_CLASS);
-    btn.classList.add(ACTIVE_FILTER_CLASS);
-  };
-
-  filterDefault.addEventListener(`click`, () => {
-    renderGallery(initialData);
-    setActiveFilter(filterDefault);
-  });
-
-  filterRandom.addEventListener(`click`, () => {
-    const randomPhotoJson = window.utils.getRandomizedArray(initialData).slice(0, RANDOM_PICTURES_COUNT);
-    renderGallery(randomPhotoJson);
-    setActiveFilter(filterRandom);
-  });
-
-  filterDiscussed.addEventListener(`click`, () => {
-    const sortedData = window.utils.getSortedCommentArr(initialData);
-    renderGallery(sortedData);
-    setActiveFilter(filterDiscussed);
-  });
-  
   // TODO move filters to module
 
   const renderGallery = (data) => { // data = xhr.response
     // Render pictures and return elements
     const pictures = window.picture.renderPictures(data); // pictures - set of elements
 
-    // Init preview listener
+    // Init Preview listener
     window.preview.setPreviewHandler(pictures, data);
 
     // Init Form listener
     window.form.setFormHandler();
+
+    // Init Filters listener
+    window.filters.setFiltersHandler(initialData);
   };
 
   const errorHandler = (errorMessage) => {
@@ -56,6 +32,7 @@
 
   // Export
   window.gallery = {
+    renderGallery,
     loadGallery,
   };
 })();
