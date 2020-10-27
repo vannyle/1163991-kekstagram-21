9e9e86a61d9d4df4430eff7f48717ad67f25f9a1
vannnyle@gmail.com <vannnyle@gmail.com>
2020-10-28 00:48:29 +0200

    Show more comments with in preview picture

--- src/js/preview.js
+++ src/js/preview.js
@@ -1,26 +1,39 @@
 const bigPicture = document.querySelector(`.big-picture`);
 const bigPictureCancel = document.querySelector(`.big-picture__cancel`);
+const moreComments = bigPicture.querySelector(`.comments-loader`);
+
+let commentsLimit = 5;
+const COMMENTS_STEP = 5;
 
 const renderBigPicture = (picture) => {
+  // Reset "more-comments" button
+  commentsLimit = 5;
+  moreComments.style.visibility = `visible`;
   bigPicture.querySelector(`.big-picture__img > img`).src = picture.url;
   bigPicture.querySelector(`.likes-count`).textContent = picture.likes;
-  bigPicture.querySelector(`.comments-count`).textContent = picture.comments.length;
+  bigPicture.querySelector(`.comments-count`).textContent = picture.comments.length.toString();
   bigPicture.querySelector(`.social__caption`).textContent = picture.description;
   bigPicture.querySelector(`.social__comments`).innerHTML = renderComments(picture.comments);
   bigPicture.classList.remove(`hidden`);
 };
 
-const renderComments = (comments, numShowComments = 5) => {
-  const showComments = comments.slice(0, numShowComments);
-  return showComments.map((comment) => (`
+const renderComment = (comment) => `
     <li class="social__comment">
       <img class="social__picture" src=${comment.avatar} alt=${comment.name} width="35" height="35">
       <p class="social__text">${comment.message}</p>
     </li>
-  `)).join(`\n`);
+  `;
+
+
+const renderComments = (comments) => {
+  const showComments = comments.slice(0, commentsLimit);
+  bigPicture.querySelector(`.comments-counter`).textContent = showComments.length;
+  return showComments.map((comment) => renderComment(comment));
 };
 
 const setPreviewHandler = (pictures, photos) => {
+  let currentPicture = null;
+
   const onUploadEscPress = (evt) => {
     if (evt.key === `Escape`) {
       evt.preventDefault();
@@ -34,11 +47,23 @@ const setPreviewHandler = (pictures, photos) => {
   // Set handler for preview
   Array.from(pictures.querySelectorAll(`.picture`)).forEach((pic, idx) => {
     pic.addEventListener(`click`, () => {
-      renderBigPicture(photos[idx]);
+      currentPicture = photos[idx];
+      renderBigPicture(currentPicture);
       document.addEventListener(`keydown`, onUploadEscPress);
     });
   });
   bigPictureCancel.addEventListener(`click`, cancelBigPicture);
+
+  moreComments.addEventListener(`click`, () => {
+    if (currentPicture) {
+      commentsLimit += COMMENTS_STEP;
+      bigPicture.querySelector(`.social__comments`).innerHTML = renderComments(currentPicture.comments);
+
+      if (currentPicture.comments.length <= commentsLimit) {
+        moreComments.style.visibility = `hidden`;
+      }
+    }
+  });
 };
 
 window.preview = {
